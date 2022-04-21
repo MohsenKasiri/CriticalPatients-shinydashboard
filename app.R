35cb65434034121208762547ffa722218c869a35
Danielle Gaudet <66831763+gaudet-d@users.noreply.github.com>
2022-04-20 22:46:35 -0400

    Update app to remove unnecessary code

--- app.R
+++ app.R
@@ -1,18 +1,14 @@
-# Palmer Penguin dashboard, using the
-# https://rstudio.github.io/shinydashboard/structure.html
+# Palmer Penguin dashboard, using RShiny
+# Shiny documentation: https://rstudio.github.io/shinydashboard/structure.html
+# Data source: https://github.com/allisonhorst/palmerpenguins 
 
 # https://fontawesome.com/icons 
 # to get icons available for use
 
-# helpful resources for making ggplots
-# http://www.sthda.com/english/wiki/ggplot2-scatter-plots-quick-start-guide-r-software-and-data-visualization
-
 # Known issues and WIPs:
-## Debug only one default selection in LDA tab
-## LDA page: find way to get errors to not show up as glaringly obvious - i.e. give helpful error message instead
-## Reduce duplication in LDA server code 
+## Reduce duplication in LDA server code.
 
-# Last updated April 1st, 2022
+# Last updated April 20th, 2022
 
 library(palmerpenguins) # penguin data
 library(ggplot2) # data visualization
@@ -29,12 +25,9 @@ library(shinydashboard)
 library(tidyverse)
 library(caret)
 library(MASS)
-#install_github("fawda123/ggord") # Install ggord
-library(ggord)
 
 data(penguins) #SOURCE: https://github.com/allisonhorst/palmerpenguins
 penguins = na.omit(penguins) #Drop 11 penguins with incomplete records.
-
 
 pendat = na.omit(penguins) #Remove 11 rows with NA for simplicity for LDA
 pendat = data.frame(pendat)
@@ -54,17 +47,11 @@ preproc.param = train.data %>%
 train.transformed = preproc.param %>% predict(train.data)
 test.transformed = preproc.param %>% predict(test.data)
 
-###Making drop down menu objects - merely for interests sake, they are not interactive at the moment
-#messageData = rbind(c("Support", "The new server is ready"), c("New User", "How do I register?"))
-#colnames(messageData) = c("from", "message")
 
 #Create a ui (user interface); commands that create the overall look of the dashboard
 ui = dashboardPage( #Create a dashboard page, uses library(shinydashboard)
-  #tags$head(
-  #  tags$link(rel="stylesheet", type="text/css", href="www/css/custom.css") #rel and type refer to the imported code? i.e. text/css says were importing a CSS file. 
-  #),
   skin="red", #Changes colour of the dashboard, use function ?validColors to see available options.
-  dashboardHeader(title = "Palmer Penguins"),
+  dashboardHeader(title = "Palmer Penguins"), #Title dashboard
   dashboardSidebar( # UI for the Sidebar
       sidebarMenu(id="menu1", #Creates a menu of tab names 
         menuItem("Exploratory Data Analysis", tabName="dashboard", icon=icon("dove")),
@@ -73,7 +60,7 @@ ui = dashboardPage( #Create a dashboard page, uses library(shinydashboard)
       ),
       #Set up a panel that only shows when on the "dashboard" tab
       conditionalPanel(condition = "input.menu1=='dashboard'", 
-        tags$hr(),
+        tags$hr(), #horizontal rule, i.e. line
         h4("Controls"),
         radioButtons(
           inputId = "species",
@@ -81,12 +68,6 @@ ui = dashboardPage( #Create a dashboard page, uses library(shinydashboard)
           choices = c("Adelie", "Chinstrap", "Gentoo", "All"),
           selected = c("Adelie")
         ),
-        #radioButtons(
-        #  inputId = "island",
-        #  label = "Island", 
-        #  choices = c("Biscoe", "Dream", "Torgersen", "All"),
-        #  selected = c("Biscoe")
-        #),
         selectInput(
           inputId = "covar1",
           label = "Variable 1",
@@ -116,9 +97,9 @@ ui = dashboardPage( #Create a dashboard page, uses library(shinydashboard)
                        )           
       )
     ),
-  dashboardBody( # UI of what appears in the mainPanel of each tab
+  dashboardBody( # UI of the mainPanel of each tab
     tabItems( # One tabItem() for each of the three tabs
-      # Exploratory data analysis
+      #TAB 1: Exploratory data analysis
       tabItem(tabName="dashboard",
               h2("Exploratory Data Analysis"),
               p("The Palmer penguins data set contains information on 344 penguins observed on three islands in the Palmer Archipelago, Antartica. The species, island, bill length, bill depth, flipper length, body mass, sex and year of observation of each penguin was recorded."),
@@ -127,54 +108,34 @@ ui = dashboardPage( #Create a dashboard page, uses library(shinydashboard)
               fluidRow(
                 tabBox(width=12,
                        id = "tabset1", #ID lets us use input$tabset1 on the server to find the current tab.
-                       #height = 250,
-                       tabPanel("By Species", tableOutput("by_species")),
-                       tabPanel("By Island", tableOutput("by_island")), #, checkboxInput("sppCheck", label="By species?")
-                       tabPanel("By Sex", tableOutput("by_sex")),
-                       tabPanel("Overall", tableOutput("crossTab"))
-                ),
-                tabBox(width=12,
-                       id="tabset2",
-                       tabPanel("Tab1", tableOutput("univarSum")),
-                       tabPanel("Tab2", tableOutput("sexSppSum"))
-                       )
-                #box(width=12, tableOutput("univarSum")) #width=12 means put onto a new line
-              ),
-              p("The following histogram and boxplot correspond to covariate 1."),
+                       tabPanel("Overall", "Count of overall penguins by sex, island and species.", tableOutput("crossTab")),
+                       tabPanel("Summary", "Summary statistics of each variable.", tableOutput("univarSum")),
+                       tabPanel("Group means", "Mean values of each variable broken in sex or species.", tableOutput("sexSppSum"))
+                )
+              ), #End of first row.
+              p("The following histogram and boxplot correspond to variable 1."),
               fluidRow(
                 box(width=5, plotOutput("hist1", height=250)), #Histogram
                 box(width=7, plotOutput("boxplot", height=250))
                 #box(width=7, plotOutput("scatter1", height=250)) #Scatterplot of two covariates
               ),
-              p("The following histogram and boxplot correspond to covariate 2."),
+              p("The following histogram and boxplot correspond to variable 2."),
               fluidRow(
                 box(width=5, plotOutput("hist2", height=250)), #Histogram
                 box(width=7, plotOutput("boxplot2", height=250)),
-                box(width=5, p("This is some text")),
+                tabBox(width=5, 
+                       tabPanel("", p("Select a tab for an introduction to the scatterplots.")),
+                       tabPanel("Below", p("The matrix of scatterplots below shows a scatterplot of each pair, the Pearson correlations in the upper right, and the variable distribution on the diagonal.")),
+                       tabPanel("Right", p("The scatterplot to the right shows the relationship between variable 1 and 2. The regression lines for each species is included, as is the overall trend line (black)."),
+                                tags$br(), p("For interest's sake: notice that in some cases the direction of the regression line of the overall data is the opposite of the lines broken down by species. This is an example of Simpson's paradox."))
+                ),
                 box(width=7, plotOutput("scatter1", height=250)) #Scatterplot of two covariates
               ),
               fluidRow(
                 box(width=12, plotOutput("ggpairplot", height=600)) #Matrix of scatterplots
               )
       ),
-      # Images of Penguins
-      tabItem(tabName="images",
-              h2("Meet the Palmer Penguins!"),
-              column(width=6, 
-                     h3("Gentoo"),
-                     img(src = "gentoo.jpg", width=300),
-                     p("https://commons.wikimedia.org/wiki/File:Gentoo_penguin.jpg")
-                     ),
-              column(width=6,
-                     h3("Adelie"),
-                     img(src = "adelie.jpg", width = 300),
-                     p("https://www.flickr.com/photos/rwoan/25706044408"),
-                     h3("Chinstrap"),
-                     img(src = "chinstrap.jpg", width = 300),
-                     p("https://www.flickr.com/photos/cmichel67/39985995")
-                     )
-      ),
-      # Linear Discriminant Analysis
+      #TAB 2: Linear Discriminant Analysis
       tabItem(tabName="analysis",
               h2("Linear Discriminant Analysis"),
               p("Use linear discriminant (LD) analysis to identify a linear combination of variables that can be used to identify the species membership of the penguins. Try different combinations of variables to see how to model accuracy and the linear discriminants change."),
@@ -194,6 +155,23 @@ ui = dashboardPage( #Create a dashboard page, uses library(shinydashboard)
               ),
               fluidRow(
                 box(width=12, plotOutput("LDA1", width=600)) 
+              )
+      ),
+      #TAB 3: Images of Penguins
+      tabItem(tabName="images",
+              h2("Meet the Palmer Penguins!"),
+              column(width=6, 
+                     h3("Gentoo"),
+                     img(src = "gentoo.jpg", width=300), #Width of image is in pixels.
+                     p("https://commons.wikimedia.org/wiki/File:Gentoo_penguin.jpg")
+              ),
+              column(width=6,
+                     h3("Adelie"),
+                     img(src = "adelie.jpg", width = 300),
+                     p("https://www.flickr.com/photos/rwoan/25706044408"),
+                     h3("Chinstrap"),
+                     img(src = "chinstrap.jpg", width = 300), 
+                     p("https://www.flickr.com/photos/cmichel67/39985995")
               )
       )
     )
@@ -236,8 +214,6 @@ server = function(input, output) {
       if(input$species == "All"){a = df} else {a = subset(df, species == input$species)}
       return(data.frame(a))
     }) 
-    #hist(df.subset()[,which(grepl(as.character(names[input$covar1]), colnames(penguins), fixed=TRUE))],
-    #     main=paste(input$species), xlab=paste(input$covar1))
     col = ifelse(input$species == "Adelie", "#F8766D", ifelse(input$species == "Chinstrap", "#00BA38", ifelse(input$species == "Gentoo", "#619CFF", "grey75")))     
     ggplot(data=df.subset(), aes(x=df.subset()[,which(grepl(as.character(names[input$covar2]), colnames(penguins), fixed=TRUE))])) +
       geom_histogram(bins=8, color="black", fill=col) + theme_classic() +
@@ -247,10 +223,13 @@ server = function(input, output) {
   output$crossTab = renderTable({
     a = ftable(penguins$species, penguins$island, penguins$sex)
     b = as.matrix(a)
-    c1 = c("Adelie", "", "", "Chinstrap", "", "", "Gentoo", "", "")
-    c2 = c("Biscoe", "Dream", "Torgersen", "Biscoe", "Dream", "Torgersen", "Biscoe", "Dream", "Torgersen")
-    tab = cbind(c1, c2, b)
-    colnames(tab) = c("Species", "Island", "Female", "Male")
+    b = b[-c(4,6,8,9),]
+    b = rbind(b, c(sum(b[,1]), sum(b[,2])))
+    c1 = c("Adelie", "", "", "Chinstrap", "Gentoo", "")
+    c2 = c("Biscoe", "Dream", "Torgersen", "Dream", "Biscoe", "TOTAL")
+    c3 = c(as.vector(b[,1]+b[,2]))
+    tab = cbind(c1, c2, b, c3)
+    colnames(tab) = c("Species", "Island", "Female", "Male", "TOTAL")
     rownames(tab) = NULL
     tab
   })
@@ -286,60 +265,23 @@ server = function(input, output) {
     colnames(mat) = c("Bill length (mm)", "Bill depth (mm)", "Flipper length (mm)", "Body mass (g)")
     mat
   }, include.rownames=T)
-  
-  #EDA: barplot changes based on covariate 1 selection -- OBSOLETE
-  output$plot2 = renderPlot({
-    df.subset2 = reactive({ #subset data based on chosen island
-      if(input$island == "All"){b = penguins} else {b = subset(penguins, island == input$island)}
-      return(b)
-    })
-    barplot(table(df.subset2()$species), main=paste("Number of Each Species:", input$island, sep=" "))
-  })
-  
-  #EDA: scatter plot of two covariates -- OBSOLETE
-  output$plot3 = renderPlot({ 
-    df = data.frame(penguins)
-    plot(df[,which(grepl(as.character(names[input$covar1]), colnames(penguins), fixed=TRUE))], df[,which(grepl(as.character(names[input$covar2]), colnames(penguins), fixed=TRUE))],
-         main=paste(input$covar1, "vs.", input$covar2, sep=" "),
-         xlab=paste(input$covar1), ylab=paste(input$covar2))
-  })
+
   
   #EDA: Make scatter plot of two covariates - broken up by species
   output$scatter1 = renderPlot({ 
     df = data.frame(penguins)
+    subdf = cbind(df[,which(grepl(as.character(names[input$covar1]), colnames(penguins), fixed=TRUE))], df[,which(grepl(as.character(names[input$covar2]), colnames(penguins), fixed=TRUE))])
+    fit = lm(subdf[,2]~subdf[,1])
     ggplot(data=df, aes(x=df[,which(grepl(as.character(names[input$covar1]), colnames(penguins), fixed=TRUE))],
                         y=df[,which(grepl(as.character(names[input$covar2]), colnames(penguins), fixed=TRUE))],
                         color=species)) + #, shape=sex
       geom_point(size=2) + 
       labs(title=paste(input$covar1, "vs", input$covar2, sep=" "), x=(paste(input$covar1)), y=(paste(input$covar2))) + 
       geom_smooth(method=lm, se=F, fullrange=T) +
-      theme_classic() + theme(text = element_text(size = 12))
+      theme_classic() + theme(text = element_text(size = 12)) +
+      geom_abline(intercept = fit$coefficients[1], slope=fit$coefficients[2], size=1.5)
   })
-  
-  #OBSOLETE --- message menu for interest's sake
-  output$messageMenu = renderMenu({
-    # Code to generate each of the messageItems here, in a list. This assumes
-    # that messageData is a data frame with two columns, 'from' and 'message'
-    msgs = apply(messageData, 1, function(row) {
-      messageItem(from = row[["from"]], message = row[["message"]])
-    })
-    dropdownMenu(type="messages", .list=msgs)
-  })
-  
-  #EDA: tables of counts, not really based on input so might be able to fix
-  output$by_species = renderTable(
-    by_species, width="100%"
-  )
-  output$by_island = renderTable(
-    #if(input$sppCheck == TRUE){
-    #  by_island = penguins %>% group_by(island, species) %>% count()
-    #  colnames(by_island) = c("Island", "Count")
-    #}
-    by_island, width="100%"
-  )
-  output$by_sex = renderTable(
-    by_sex, width="100%"
-  )
+
   
   #EDA: boxplot of covariate 1 broken down by species and island.
   output$boxplot = renderPlot({
@@ -350,12 +292,12 @@ server = function(input, output) {
       theme(text = element_text(size = 12))
   })
   
-  #EDA: boxplot of covariate 1 broken down by species and island.
+  #EDA: boxplot of covariate 2 broken down by species and island.
   output$boxplot2 = renderPlot({
     df = data.frame(penguins)
     ggplot(df, aes(y=df[,which(grepl(as.character(names[input$covar2]), colnames(penguins), fixed=TRUE))],
                    x=island, fill=species)) + geom_boxplot() + theme_classic() +
-      labs(title=paste("Distributions of", input$covar1, "for species by island", sep=" "), x="Island", y=paste(input$covar2)) +
+      labs(title=paste("Distributions of", input$covar2, "for species by island", sep=" "), x="Island", y=paste(input$covar2)) +
       theme(text = element_text(size = 12))
   })
   
@@ -466,16 +408,5 @@ server = function(input, output) {
   })
 }
 
-shinyApp(ui,server) #Creates a Shiny app object from a ui/server pair
-
-
-# Extra bits
-#box(tags$div( #Creates a section (e.g., "division") of an HTML document. divs provide a useful hook for CSS styling. You can use the helper function div.
-#  "Some text followed by a break", 
-#  tags$br(),
-#  tags$i("Some text following a break")
-#))
-
-#Test penguin histogram
-
-#g = ggplot(penguins, aes(x=df[,which(grepl(as.character(names[input$covar1]), colnames(penguins), fixed=TRUE))]))
+# Final call to tie the UI and server together. 
+shinyApp(ui,server) # Creates a Shiny app object from a ui/server pair
